9e5f2517c32e5d021fbd66bbc5d14888f45e81b8
tedlopus <tedlopus@gmail.com>
2022-06-06 00:21:27 -0600

    added and updated footer and header components and css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import '../styles/Header.css';
 import gitHub from '../assets/logos/GitHub-Mark-120px-plus.png';
 import linkedIn from '../assets/logos/LI-In-Bug.png';
 import stackOverflow from '../assets/logos/logo-stackoverflow.png';
 
-export default function Footer() {
+const styles = {
+  footerStyle: {
+    background: 'blue',
+  },
+};
+
+function Footer() {
   return (
-    <div class="row justify-content-evenly">
+    <footer style={styles.footerStyle} className="footer">
+      <div class="row justify-content-evenly">
       <div class="col text-center">
         <a href="https://github.com/tedlopus" class="text-decoration-none"><img class="logo" alt="gitHub logo" src={gitHub}></img></a>
       </div>
@@ -16,5 +24,8 @@ export default function Footer() {
         <a href="/Users/edwardlopus/Downloads/Resume-Edward-Lopus-Code.pdf" class="text-decoration-none"><img class="logo" alt="stackOverflow logo" src={stackOverflow}></img></a>
       </div>
     </div>
+    </footer>
   );
 }
+
+export default Footer;
